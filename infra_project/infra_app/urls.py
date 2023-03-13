d1be3882919f5e75b46f04ca1d0f6b81d3a5d818
taube-a <Taube.Anastasia@yandex.ru>
2023-03-13 12:39:19 +0300

    fix according tests

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'infra_app'
 
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('second/', views.second_page, name='second_page'),
+urlpattern = [
+    path('/', views.index, name='index'),
+    path('second_page/', views.second_page, name='second_page'),
 
 ]
